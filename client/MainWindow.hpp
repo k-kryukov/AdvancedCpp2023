a61584420f73917b58a6051f40f117d3b2255304
Konstantin Kryukov <k.kryukov@yadro.com>
2023-12-12 17:57:06 +0300

    added Refresh feature in client

--- client/MainWindow.hpp
+++ client/MainWindow.hpp
@@ -1,25 +1,54 @@
 #pragma once
 
 #include <vector>
+#include <thread>
+
 #include <QMainWindow>
 #include <QWidget>
 #include <QBoxLayout>
 #include <QLineEdit>
 #include <QMessageBox>
 #include <QPushButton>
+#include <QListWidget>
+#include <QLabel>
 #include <glog/logging.h>
 
 #include "Service.hpp"
 
 class MainWindow : public QObject
 {
-    QPushButton button;
+    QPushButton exitButton;
+    QPushButton refreshButton;
     QVBoxLayout layout;
     QWidget window;
     Service service;
     std::string currentUser_;
     std::string currentPassword_;
     std::vector<std::string> notes_;
+    std::vector<QLabel*> labels_;
+
+public:
+    MainWindow() {}
+
+    void emplaceNotes(std::vector<std::string> notes) {
+        for (auto label : labels_) {
+            layout.removeWidget(label);
+        }
+
+        std::for_each(labels_.begin(), labels_.end(), [] (auto widget) { delete widget; });
+        labels_.clear();
+
+        for (auto&& note : notes) {
+            QLabel *label = new QLabel(note.data(), &window);
+            layout.addWidget(label);
+            labels_.push_back(label);
+        }
+    }
+
+private:
+    auto fetchNotesFromServer() {
+        return service.getNotes(currentUser_, currentPassword_);
+    }
 
 private slots:
     void exitButtonPushed() {
@@ -28,22 +57,25 @@ private slots:
         ::exit(0);
     }
 
-    auto fetchNotesFromServer() {
-        return service.getNotes(currentUser_, currentPassword_);
+    void refreshButtonPushed() {
+        LOG(INFO) << "Refresh button is pushed!";
+
+        auto notes = fetchNotesFromServer();
+        emplaceNotes(std::move(notes));
     }
 
 public:
-    MainWindow() {}
-
-    void emplaceNotes(std::vector<std::string> notes) { LOG(INFO) << "Emplacing notes!"; }
-
     void init(std::string username, std::string password) {
         currentUser_ = std::move(username);
         currentPassword_ = std::move(password);
 
-        layout.addWidget(&button);
-        button.setObjectName("pushButton");
-        button.setText("Exit");
+        layout.addWidget(&exitButton);
+        layout.addWidget(&refreshButton);
+        exitButton.setObjectName("pushButton");
+        exitButton.setText("Exit");
+
+        refreshButton.setObjectName("refreshButton");
+        refreshButton.setText("Refresh");
 
         window.setLayout(&layout);
         window.setFixedSize(800, 800);
@@ -51,7 +83,8 @@ public:
         auto notes = fetchNotesFromServer();
         emplaceNotes(std::move(notes));
 
-        QObject::connect(&button, &QPushButton::clicked, this, &MainWindow::exitButtonPushed);
+        QObject::connect(&exitButton, &QPushButton::clicked, this, &MainWindow::exitButtonPushed);
+        QObject::connect(&refreshButton, &QPushButton::clicked, this, &MainWindow::refreshButtonPushed);
     }
 
     void show() { window.show(); }
