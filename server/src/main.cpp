820a93b4099070e97b54ee539f7c9bbeb3338a73
Konstantin Kryukov <k.kryukov@yadro.com>
2023-12-22 19:54:14 +0300

    added ex handling

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -3,12 +3,16 @@
 #include "Server.hpp"
 
 int main(int argc, char** argv) {
-  google::InitGoogleLogging(argv[0]);
-  FLAGS_alsologtostderr = 1;
+    google::InitGoogleLogging(argv[0]);
+    FLAGS_alsologtostderr = 1;
 
-  Server server{12345};
-  server.start();
-  server.start_ctx();
+    try {
+        Server server{12345};
+        server.start();
+        server.start_ctx();
+    } catch (std::exception const& ex) {
+        LOG(FATAL) << "Error happened during server starting: " << std::move(ex.what());
+    }
 
-  return 0;
+    return 0;
 }
